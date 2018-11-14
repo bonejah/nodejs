c2ea818619e877664d3c877acfc0c344ed43a7a3
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-11-14 17:56:19 -0200

    add node-str

---
+++ node-str/src/app.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const router = express.Router()
+
+const indexRoute = require('./routes/index-route')
+const productRoute = require('./routes/product-route')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', indexRoute)
+app.use('/products', productRoute)
+
+module.exports = app;
