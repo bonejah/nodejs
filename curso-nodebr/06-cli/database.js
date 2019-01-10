a3c2322c60a7b966a5eab18d1dc99e52d729f075
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2019-01-10 14:36:35 -0200

    add module cli

--- curso-nodebr/06-cli/database.js
+++ curso-nodebr/06-cli/database.js
@@ -41,6 +41,23 @@ class DataBase {
     const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
     return dadosFiltrados
   }
+
+  async remover(id) {
+    if (!id) {
+      return await this.escreverArquivo([])
+    }
+
+    const dados = await this.obterDadosArquivo()
+    console.log('id', id)
+    const indice = dados.findIndex(item => item.id === parseInt(id))
+
+    if (indice === -1) {
+      throw Error('O usuario informado nao existe!')
+    }
+
+    dados.splice(indice, 1)
+    return await this.escreverArquivo(dados)
+  }
 }
 
 
--- curso-nodebr/06-cli/test.js
+++ curso-nodebr/06-cli/test.js
@@ -24,4 +24,11 @@ describe('Suite de manipulacao de herois', () => {
     const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
     deepEqual(atual, expected)
   })
+
+  it('deve remover um heroi por id', async () => {
+    const expected = true
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+    deepEqual(resultado, expected)
+  })
+
 })
